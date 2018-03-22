db140d64bbc1a9670b7359956b80de4fd00fd1a1
antoniommguedes <antonio.miguel@acad.pucrs.br>
2018-03-22 20:37:18 -0300

    Versão1 sem case tests.

---
+++ Aula_1/src/App.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		// Cliente a = new Cliente("a", 1);
+		// Cliente b = new Cliente("b", 2);
+		// Cliente c = new Cliente("c", 80);
+		// Cliente d = new Cliente("d", 4);
+		// Cliente e = new Cliente("e", 5);
+
+		Banco banco = new Banco();
+		// banco.addCliente(a);
+		// banco.addCliente(b);
+		// banco.addCliente(c);
+		// banco.addCliente(d);
+		// banco.addCliente(e);
+
+		// banco.proximoDaFila(4);
+		// banco.getPessoasNaFila();
+
+		Scanner ler = new Scanner(System.in);
+
+	
+		int opcao;
+		
+		do {
+			System.out.println("Opção 1 - Próximo da Fila");
+			System.out.println("Opção 2 - Cadastrar Cliente");
+			System.out.println("Opção 3 - Ver clientes na fila");
+			System.out.println("Opção 4 - Encerra sistema");
+			opcao = ler.nextInt();
+			switch (opcao) {
+
+			case 1:
+				System.out.println("Informe o Numero do Caixa");
+				int n = ler.nextInt();
+				banco.proximoDaFila(n);
+				
+				break;
+			case 2:
+				System.out.println("Informe o nome: ");
+				String nome = ler.next();
+				System.out.println("Informe a idade: ");
+				int idade = ler.nextInt();
+				Cliente a = new Cliente(nome, idade);
+				banco.addCliente(a);
+				
+				break;
+			case 3:
+				banco.getPessoasNaFila();
+			
+				break;
+			}
+
+		} while (opcao != 4);
+	}
+
+}
---
+++ Aula_1/src/Banco.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Banco {
+
+	private ArrayList<Cliente> filaDeClientes;
+
+	public Banco() {
+		filaDeClientes = new ArrayList<Cliente>();
+	}
+
+	public void addCliente(Cliente cliente) {
+		filaDeClientes.add(cliente);
+	}
+
+	public void proximoDaFila(int nroCaixa) {
+		if (nroCaixa > 0 && nroCaixa <= 5) {
+			for (Cliente c : filaDeClientes) {
+				if (c.getIdade() >= 65) {
+					filaDeClientes.remove(c);
+					return;
+				}
+			}
+		}
+			filaDeClientes.remove(0);
+	}
+
+	public void getPessoasNaFila() {
+		for (Cliente c : filaDeClientes) {
+			System.out.println(c.getNome() + ", " + c.getIdade());
+		}
+	}
+	
+}
